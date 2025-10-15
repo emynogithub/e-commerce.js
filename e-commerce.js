0b616a2c8977c98536422401cf16dc0ebf243562
Godwin Andrew <andrewchiemelie99@gmail.com>
2025-10-15 10:05:12 +0100

    doc(assignment): add the questions

---
+++ e-commerce.js
@@ -0,0 +1,19 @@
+// # Take-Home Assessment: The E-Commerce Data Report
+// # Objective: To demonstrate mastery of functional array methods by processing a complex
+// # dataset to generate a specific report object.
+// # The Task: You are given an array of transaction objects for an e-commerce store. Your
+// # task is to write a script ecommerce-report.js that uses a single chain of array methods
+// # to calculate a report object.
+// # Starter Code:
+// # // ecommerce-report.js
+// # const transactions = [
+// # { id: 'tr_1', customerId: 'c_1', items: [{ productId: 'p_1', price: 50, quantity: 2 { id: 'tr_2', customerId: 'c_2', items: [{ productId: 'p_3', price: 120, quantity: { id: 'tr_3', customerId: 'c_1', items: [{ productId: 'p_2', price: 25, quantity: 3 { id: 'tr_4', customerId: 'c_3', items: [{ productId: 'p_4', price: 80, quantity: 1 { id: 'tr_5', customerId: 'c_1', items: [{ productId: 'p_1', price: 50, quantity: 1 ];
+// # Your Goal: Generate a report object that contains two properties: 1. totalRevenue:
+// # The sum of the total cost of all completed transactions. The total cost of a transaction
+// # is the sum of price * quantity for all its items. 2. customers: An object where each
+// # key is a customerId and the value is the total amount they have spent on completed
+// # transactions.
+
+
+
+
